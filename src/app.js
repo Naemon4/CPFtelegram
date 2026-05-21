ea5f27d0aea031ba51805a8d3976b5b5b070a0d7
Pedro Henrique Silva Pimenta <160595738+Pimentaah@users.noreply.github.com>
2026-05-21 01:44:13 +0000

    feat: Integrar bot

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
 require('dotenv').config();
-const TelegramBot = require("node-telegram-bot-api")
-//const ValidarCpf = require("/.BotService") adicionar linha dps que for feito o service
+const TelegramBot = require('node-telegram-bot-api');
+const validarCPF = require('./validarCPF');
+
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id,
+    '👋 Olá! Me envie um CPF para validar.\nPode ser com ou sem formatação.\nEx: 123.456.789-09'
+  );
+});
+
+//bot.on('message', (msg) => {
+
+//  } coloque o conteúdo aqui dentro pra o bot funcionar
+//});
+
+console.log('Bot rodando...');
